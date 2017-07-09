87b19cf4e6a21bfe0f19df531abe4034e9514a8e
Patrick Lafferty <patrick_lafferty@outlook.com>
2017-07-08 22:04:23 -0400

    wrote the first version of the autocompleter: only returns the first level matches
    so autocompleting "defi" would give define and not define-syntax as that would be nested under define

--- src/editor/autocompletion.rkt
+++ src/editor/autocompletion.rkt
@@ -1,3 +1,22 @@
+#|
+MIT License
+Copyright (c) 2017 Patrick Lafferty
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+|#
 #lang typed/racket/base
 
 (require "../functional/maybe.rkt"
@@ -8,142 +27,11 @@
 (define-type Trie trie-node)
 (struct trie-node (
     [label : (Maybe (Listof Char))]
-    ;[children : (HashTable Char Trie)])
     [children : (Listof Trie)])
     #:transparent)   
 
 (define known-identifiers (trie-node (some '()) '()))
-#|
-find tellephone (te (st ll)) 0
 
-  te llephone '()
-  find llephone (st ll) 0
-    find llephone (ll) 1
-        ll ephone ()
-        find ephone ()
-            none (couldnt find in any children)
-        if has children
-            add-leaf
-            replace-node
-        
-
-
-
-
-|#
-#|(struct find-result (
-    [common : (Listof Char)] ;common prefix between target identifier and child label
-    [rest-identifier : (Listof Char)] ; the remaining part of identifier after common prefix
-    [rest-child : (Listof Char)] ;the remaining part of child label after common prefix
-    [child-index : Integer] ;the index of the child that this identifier matched
-    ;[breadcrumbs : (Listof Integer)]
-) #:transparent)
-
-(: fmap-cons (-> find-result (Maybe (Listof find-result)) (Maybe (Listof find-result))))
-(define (fmap-cons a b)
-    (some 
-        (match b
-            [(some x) (cons a x)]
-            [(none) (cons a empty)])))|#
-#|
-how does this work:
-
-if x and y share a common prefix,
--if (rest x) and any child of y share a common prefix ^
-
-test 
-+ tell
--shares 'te', trie is just test
-replace 'test' with 'te' ['st', 'll']
-
-what does find-result need:
-
-node:
--common prefix
--child index to replace
-
-leaf:
--rest-id
--rest-child
-
-type find-result =
-    | node common-prefix child-index
-    | leaf rest-id rest-child
-
-(find-result te 1
-    (find-result l 
-        l
-        lephone)
-
-[te [st, ll]] => [te [st, l [l, ephone]]]
-[te [st, ll]] => [te, [st, ll, am]]
-+telephone
--shares 'te'
--shares 'l'
-replace 'll' with 'l'['l', 'ephone']
-
-+television
--shares 'te'
--shares 'l'
--shares 'e'
-'te' ['st', 'l' ['e' ['phone', 'vision'], 'l']
-
-+telling
-'te' ['st, 'l'
-    ['e' ['phone', 'vision']]
-    ['l' [null, 'ing']]
-
-|#
-#|(define (find-common-prefix 
-            [identifier : (Listof Char)] 
-            [children : (Listof Trie)]
-            [child-index : Integer]) : (Maybe (Listof find-result))
-    (if (null? children)
-        (none)
-        (let-values ([(common rest-id rest-child) 
-                        (split-common-prefix identifier (trie-node-label (first children)))])
-            (if (null? common)
-                (find-common-prefix identifier (rest children) (add1 child-index))
-                ;(some (find-result common rest-id rest-child child-index))))))
-                (if (null? rest-id)
-                    ;(some (cons (find-result common rest-id rest-child child-index)) empty)
-                    (fmap-cons (find-result common rest-id rest-child child-index) (none))
-                    ;(cons (find-result common rest-id rest-child child-index) (find-common-prefix rest-id (trie-node-children (first children)))))))))
-                    (fmap-cons 
-                        (find-result common rest-id rest-child child-index) 
-                        (find-common-prefix rest-id 
-                            (trie-node-children (first children))
-                            0
-                            )))))))
-                            |#
-;type find-result =
-    ;| node common-prefix child-index
-    ;| leaf rest-id rest-child
-#|
-
-type find-result =
-    | 'exists
-    | add-leaf id
-    | split-node common rest-id rest-child
-    | update-child index
-
-'te' ['st', 'lling'] + tell
-
-(update-child 1) (replace-node 'll' '' 'ing)
-
-'te' ['st', 'll'] + telephone
-
-((update-child 1) (split-node 'l' 'ephone' 'l'))
-
-'te' ['st', 'l' ['l', 'ephone']] + telling
-
-(update-child 1) (add-leaf ing)
-
-'te' ['st', 'l' ['l' [null, 'ing'], 'ephone']] + television
-
-(update-child 1) (update-child 1) (split-node 'e' 'vision' 'phone')
-
-|#
 (struct update-child (
     [child-index : Integer]
 ) #:transparent)
@@ -160,7 +48,11 @@ type find-result =
     [rest-identiifer : (Listof Char)]
 ) #:transparent)
 
-(define-type FindResult (U update-child replace-node add-leaf 'exists))
+(struct exists (
+    [child-index : Integer]
+)#:transparent)
+
+(define-type FindResult (U update-child replace-node add-leaf exists))
 
 (define (find
     [identifier : (Listof Char)]
@@ -184,8 +76,7 @@ type find-result =
         [(some '()) 
             (let ([result (find-children identifier (trie-node-children trie) 0 0)])
                 (match result 
-                    ;[(cons child-index result) (some (list (update-child child-index) result))]))]
-                    [(cons _ (some (list 'exists))) (some (list 'exists))]
+                    [(cons x (some (list (exists y)))) (some (cons (exists x) (list (exists y))))]
                     [(cons -1 (some change)) (some change)]
                     [(cons x (some change)) (some (cons (update-child x) change))]))]
         [(some label)
@@ -199,89 +90,31 @@ type find-result =
                         ;end of search word reached
                         (if (null? rest-label)
                             ;end of label reached
-                            (some (list 'exists))
+                            ;(some (list 'exists))
+                            (some (list (exists parent-index)))
+                            #|(if ((inst findf Trie) (lambda (t) (none? (trie-node-label t))) (trie-node-children trie))
+                                (some (list 'exists))
+                                (none))|#
                             ;end of search word but label still has more letters, split label
                             (some (list (replace-node parent-index common rest-id rest-label))))
                         ;more left in search word
                         (if (null? rest-label)
                             ;more left in search word but label is a proper prefix of search word
-                            ;(some (list (replace-node parent-index common rest-id rest-child)))
                             (let ([result (find-children rest-id (trie-node-children trie) 0 parent-index)])
                                 (match result
-                                    [(cons _ (some (list 'exists))) (some (list 'exists))]
+                                    [(cons x (some (list (exists y)))) (some (cons (exists x) (list (exists y))))]
                                     [(cons -1 (some change)) (some change)]
                                     [(cons x (some change)) (some (cons (update-child x) change))]))
-                            ;more left in both, find a child that can complete the search word
-                            ;(let ([result (find rest-id )]))
                             (some (list (replace-node parent-index common rest-id rest-label)))))))]))
-       ;telll + tell => tel ll l             
 
-        ;end of word
-        ;[(none) (some (list (replace-node parent-index identifier)))]
-
-
-    #|(if (null? (trie-node-children trie))
-        (some (list (add-leaf parent-index identifier)))
-        (let-values ([(common rest-id rest-child)
-                        (split-common-prefix identifier (trie-node-label (first children)))])
-            (if (null? common)
-                ;this child has nothing in common, look at the other children
-                (let ([result (find identifier (rest children) (add1 child-index))])
-                    (match result 
-                        [(some _ ) result]
-                        ;should never be none
-                        [(none) (some (list (add-leaf child-index rest-id)))]))
-                (if (null? rest-id)
-                    (if (null? rest-child)
-                        (some (list 'exists))
-                        (some (list (replace-node child-index common rest-id rest-child))))
-                    ;we have something in common, theres still something in id remaining
-                    (let ([result (find rest-id (trie-node-children (first children)) 0)])
-                        (match result
-                            [(some result)
-                                (match result
-                                    [(list 'exists ) (some (list 'exists))]
-                                    [(list (replace-node index _ _ _))
-                                        (some (cons (update-child index) result))]
-                                    [_ 
-                                        (some (cons (update-child child-index) result))])]
-                            [(none)
-                                (if (null? (trie-node-children (first children)))
-                                    (some (list (replace-node child-index common rest-id rest-child)))
-                                    (some (list (add-leaf child-index rest-id))))]
-                            )))))))|#
-                        
-
-#|(define test (trie-node empty
-    (list (trie-node (string->list "te") 
-        (list (trie-node (string->list "st") empty)
-                (trie-node (string->list "ll") empty) 
-    
-    )))))
-|#
-#|
-te 
-    [st ll]
-
-+telephone
-
-(update-child 1) (replace-node 1 l ephone l)
-
-te
-    [st l]
-        [lephone l]
-
-|#
-;(find (string->list "tell") (trie-node-children test) 0) 
 (define (replace 
         [trie : Trie]
         [breadcrumbs : (Maybe (Listof FindResult))]) : Trie
     (match breadcrumbs
         [(none) trie]
-        [(some (list 'exists)) trie]
+        [(some (list (exists _))) trie]
         [(some (cons (update-child index) remaining))
             (let ([children (trie-node-children trie)])
-                   ;[trie (if (< index 0) trie (list-ref children index))])
                 (struct-copy trie-node trie
                     [children (list-set children index (replace (list-ref children index) (some remaining)))]))]
         [(some (cons (replace-node child-index common rest-id rest-child) _))
@@ -293,88 +126,20 @@ te
         [(some (list (add-leaf child-index rest-id)))
             (struct-copy trie-node trie
                 [children (cons (trie-node (some rest-id) empty) (trie-node-children trie))])]))
-        #|[(cons (find-result common rest-id rest-child index) empty)
-            (let* ([child-to-replace (list-ref children index)])
-
-                (struct-copy trie-node child-to-replace
-                    [label common]
-                    [children (list (trie-node rest-id '()#|empty|#) (trie-node rest-child '()#|empty|#))]))]
-
-        [(cons (find-result common _ _ index) others)
-            (let* ([child-to-replace (list-ref children index)]
-                    [childs-children (trie-node-children child-to-replace)])
-
-                (struct-copy trie-node child-to-replace
-                    [label common]
-                    [children (list-set children index (replace childs-children (rest breadcrumbs)))]))]))
-                    |#
-
-#|(define (insert 
-    [identifier : (Listof Char)]
-    [trie : Trie]) : Trie
-    
-    (let ([children (trie-node-children trie)])
-        (if (null? children)
-            (struct-copy trie-node trie 
-                [children (list (trie-node identifier empty))])
-            (match (find-common-prefix identifier children 0)
-                [(some breadcrumbs)
-                    ;(struct-copy trie-node trie
-                    (replace children breadcrumbs)]
-|#
 
 (define (insert
     [identifier : (Listof Char)]
     [trie : Trie]) : Trie
     (let ([breadcrumbs (find identifier trie 0)])
-        (printf "breadcrumbs: ~v~n" breadcrumbs)
         (replace trie breadcrumbs)))
-                #|[(some (find-result common rest-id rest-child index))
-                    (if (null? rest-id)
-                        (let ([replaced-child 
-                                (trie-node common 
-                                    (list (trie-node rest-id empty) 
-                                            (trie-node rest-child (trie-node-children (list-ref children index)))))])
-                            (struct-copy trie-node trie
-                                [children (list-set children index replaced-child)]))
-                        (let* ([result (insert rest-id (list-ref children index))]
-                                [replaced-subtrie 
-                                (struct-copy trie-node result
-                                    [label common]
-                                    [children (cons (trie-node rest-child empty) (trie-node-children result))])])
-                            (struct-copy trie-node trie
-                                [children (list-set children index replaced-subtrie)])))]|#
-                    
-              #|  [(none)
-                    (let ([new-child (trie-node identifier empty)])
-                        (struct-copy trie-node trie
-                            [children (cons new-child children)]))]))))
-|#
+
 (: print-trie (->* (Trie) (Integer) Void))
 (define (print-trie 
     trie [indent 0])
-    ;[trie : Trie] 
-    ;[indent : Integer 0]) : Void
     (printf "~a~v~n" (make-string indent #\space) (trie-node-label trie))
     (for ([child : Trie (trie-node-children trie)])
         (print-trie child (add1 indent))))
-#|
-add test:
-test
-----
-add tell:
-te
-    st ll
---------
-add telephone:
-te
-    st l
-        l   lephone
-(print-trie 
-    (insert (string->list "telephone")
-        (insert (string->list "tell") 
-            (insert (string->list "test") known-identifiers))))
-|#
+
 (define interpreter-keywords (list 
     'if
     'or
@@ -393,11 +158,56 @@ te
     'when
     'unless
     ))
-(print-trie
+
+(let ([thing
+
 (for/fold ([trie : Trie (trie-node (some '()) empty)])
     ([id : Symbol interpreter-keywords])
-    (writeln trie)
-    (writeln "")
-    (insert (string->list (symbol->string id)) trie)))
+    (insert (string->list (symbol->string id)) trie))])
+(set! known-identifiers thing)
+)
 
-;(define (autocomplete characters)
+(define (flatten-trie
+    [trie : Trie]) : (Listof Trie)
+
+    (if (null? (trie-node-children trie))
+        (cons trie empty)
+        (cons trie 
+            (for/fold ([acc : (Listof Trie) '()]) ([child : Trie (trie-node-children trie)]) ;(flatten-trie (trie-node-children trie))])
+                (let ([subtrie : (Listof Trie) (flatten-trie child)])
+                    (for/fold ([acc : (Listof Trie) acc]) ([c subtrie])
+                        (cons c acc)))))))
+
+(define (autocomplete 
+    [characters : (Listof Char)]) : (Maybe (Listof (Listof Char)))
+
+    (define (traverse 
+        [breadcrumbs : (Listof FindResult)] 
+        [trie : Trie]) : Trie
+        (match breadcrumbs
+            [(cons (update-child index) remaining)
+                (traverse remaining (list-ref (trie-node-children trie) index))]
+            [(cons (exists index) remaining)
+                (if (null? remaining)
+                    trie
+                    (traverse remaining (list-ref (trie-node-children trie) index)))]
+            [_ trie]))
+
+    (let ([breadcrumbs (find characters known-identifiers 0)])
+        (match breadcrumbs 
+            [(none) (none)]
+            [(some (cons (replace-node _ _ _ _) _)) (none)]
+            [(some (cons (add-leaf _ _) _)) (none)]
+            [(some thing)
+                (let* ([subtrie : Trie (traverse thing known-identifiers)]
+                        [completions : (Listof (Listof Char)) 
+                            (for/fold ([acc : (Listof (Listof Char)) '()]) ([trie : Trie (trie-node-children subtrie)])
+                                (match (trie-node-label trie)
+                                    [(some x) (cons x acc)]
+                                    [(none) acc]))])
+                    ((inst some (Listof (Listof Char))) completions))]
+        )))
+
+                    
+;TODO: consider having function keys (f1-f4) switch tabs in the completion popup window
+;that can filter between types functions methods properties etc
